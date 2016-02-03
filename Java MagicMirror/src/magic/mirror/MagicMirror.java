59101c1b6ea70e7cce56ab874dfd80e0deddd381
welcome-2themachine <Christopher.prevost94@gmail.com>
2016-02-03 16:47:13 -0500

    Branch Test

---
+++ Java MagicMirror/src/magic/mirror/MagicMirror.java
@@ -0,0 +1,16 @@
+package magic.mirror;
+
+/**
+ *
+ * @author chris
+ */
+public class MagicMirror {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
