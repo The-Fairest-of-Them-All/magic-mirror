9aaa99f7c5818bc11a1b3f34d6d43babd35c4fa3
Raymond Jackson <rjackson1989@temple.edu>
2016-02-08 13:07:23 -0500

    main activity Layout completed

--- magicMirror/app/src/main/java/fairestintheland/magicmirror/MainActivity.java
+++ magicMirror/app/src/main/java/fairestintheland/magicmirror/MainActivity.java
@@ -1,16 +1,94 @@
 package fairestintheland.magicmirror;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    private ArrayList<Switch> theSwitches;
+    private MenuAdapter<Switch> adapter;
+    private ListView navList;
+    private Button sleepButton;
+    private boolean sleeping;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sleeping = false;
+        theSwitches = new ArrayList<>();
+        initSwitches();
+        navList = (ListView) findViewById(R.id.left_drawer);
+        adapter = new MenuAdapter<>(this, android.R.layout.simple_list_item_1, theSwitches);
+        navList.setAdapter(adapter);
+        sleepButton = (Button)findViewById(R.id.sleepButton);
+        sleepButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sleeping = !sleeping;
+
+                if(sleeping)
+                {
+                    sleepButton.setText(R.string.wake_button);
+                }
+                else
+                {
+                    sleepButton.setText(R.string.sleep_button);
+                }
+            }
+        });
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+    }
+
+    private void initSwitches()
+    {
+        String[] switchNames = getResources().getStringArray(R.array.settings);
+
+        for(String name : switchNames)
+        {
+            Switch s = new Switch(this);
+            s.setText(name);
+            theSwitches.add(s);
+        }
+
     }
 
     @Override
@@ -33,5 +111,67 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static class ViewHolder
+    {
+        public TextView text;
+        public Switch toggle;
+    }
+
+    class MenuAdapter<T> extends ArrayAdapter<Switch>
+    {
+        List<Switch> list;
+        Context context;
+
+        public MenuAdapter(Context context, int resource, List<Switch> switches) {
+            super(context, resource, switches);
+            this.context = context;
+            list = switches;
+        }
+
+        @Override
+        public int getCount()
+        {
+            return list.size();
+        }
+        @Override
+        public Switch getItem(int position)
+        {
+            return list.get(position);
+        }
+        @Override
+        public long getItemId(int position)
+        {
+            return position;
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            View v = convertView;
+
+            ViewHolder holder;
+            // First let's verify the convertView is not null
+            if (convertView == null) {
+                // This a new view we inflate the new layout
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                v = inflater.inflate(R.layout.switch_row_layout, null);
+                holder = new ViewHolder();
+                holder.text =(TextView) v.findViewById(R.id.switch_name);
+                holder.toggle = (Switch) v.findViewById(R.id.prefSwitch);
+                v.setTag(holder);
+            }
+
+            else
+            {
+                holder = (ViewHolder)v.getTag();
+            }
+            // Now we can fill the layout with the right values
+            holder.text.setText(list.get(position).getText());
+            holder.toggle = list.get(position);
+
+            return v;
+
+        }
     }
 }
