2b9068c5d6eeb7010dab8227e0f521a7b9dafbd1
snowman1ng <tue99667@temple.edu>
2016-03-09 15:00:12 -0500

    Added code to connect to a socket from raspberryPiServer in the TestConnection/THISVERSIONWORKS folder into RPiUI. RPiUI now displays the UI in one thread and waits for a connection in a second thread.

--- RpiUI/src/rpiui/RpiUI.java
+++ RpiUI/src/rpiui/RpiUI.java
@@ -7,11 +7,22 @@ package rpiui;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.GraphicsDevice.*;
+import java.net.*;
+import java.io.*;
+import java.util.Enumeration;
 /**
  *
  * @author KatieHorn
  */
 public class RpiUI extends JFrame {
+    
+    static final int BITMAP_SIZE = 8;
+    static final int SocketServerPORT = 55555;
+    
+    static ServerSocket serverSocket;
+    static String ipAddress;
+    static String message = "";
+    static Thread socketServerThread;
 
     /**
      * @param args the command line arguments
@@ -25,16 +36,11 @@ public class RpiUI extends JFrame {
             + "anim id est laborum.";
     static String quote = "I'm ready - Spongebob Squarepants";
     
-<<<<<<< HEAD
-    public static void framer(){
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-=======
+
     private static void framer(){
         JFrame frame = new JFrame("Raspberry Pi App");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
->>>>>>> 65fa736e0cbe17570f67a6c58337e746b903b768
         frame.setUndecorated(true);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setPreferredSize(screenSize);
@@ -64,7 +70,6 @@ public class RpiUI extends JFrame {
         twitc.fill = GridBagConstraints.VERTICAL;
         twitc.anchor = GridBagConstraints.FIRST_LINE_END;
         
-<<<<<<< HEAD
         JLabel blank = new JLabel();
    //     blank.setForeground(Color.red);
        JLabel blank2 = new JLabel();
@@ -77,12 +82,7 @@ public class RpiUI extends JFrame {
       //  blank5.setForeground(Color.red);
         JLabel blank6 = new JLabel();
   //      blank6.setForeground(Color.red);
-=======
-        JLabel blank = new JLabel("testing 1 2 3");
-        blank.setForeground(Color.blue);
-        JLabel blank2 = new JLabel("test");
-        blank2.setForeground(Color.red);
->>>>>>> 65fa736e0cbe17570f67a6c58337e746b903b768
+  
         GridBagConstraints topmiddlec = new GridBagConstraints();
         topmiddlec.gridy = 0;
         topmiddlec.gridx = 1;
@@ -174,7 +174,7 @@ public class RpiUI extends JFrame {
         frame.setVisible(true);
         
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         // TODO code application logic here
         
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
@@ -182,6 +182,207 @@ public class RpiUI extends JFrame {
                 framer();
             }
         });
+        
+        System.out.println("Starting raspberry pi program.");
+        ipAddress = getIpAddress();
+        System.out.println("My IP address is: " + ipAddress);
+        socketServerThread = new Thread(new SocketServerThread());
+        socketServerThread.start();
+    }
+    
+    private static class SocketServerThread extends Thread {
+        int count = 0;
+
+        @Override
+        public void run() {
+            Socket socket = null;
+            DataInputStream dataInputStream = null;
+            DataOutputStream dataOutputStream = null;
+
+            try {
+                serverSocket = new ServerSocket(SocketServerPORT);
+
+                System.out.println("I'm waiting here: " + serverSocket.getLocalPort());
+                while (true) {
+                    socket = serverSocket.accept();
+                    dataInputStream = new DataInputStream(
+                            socket.getInputStream());
+                    dataOutputStream = new DataOutputStream(
+                            socket.getOutputStream());
+
+                    String messageFromClient = "";
+
+                    //If no message sent from client, this code will block the program
+                    messageFromClient = dataInputStream.readUTF();
+
+                    count++;
+                    message += "#" + count + " from " + socket.getInetAddress()
+                            + ":" + socket.getPort() + "\n"
+                            + "Msg from client: " + messageFromClient + "\n";
+
+                    System.out.println(message);
+                    String msgReply = "Hello from Raspberry Pi (Regular computer right now), you are #" + count;
+                    dataOutputStream.writeUTF(msgReply);
+                    
+                    /*
+                    //TODO comment the rest of the try block out up to --- for code to run as before
+                    //-----------------------------------------------------------------------------------
+                    //request preferences count and actual preferences
+                    msgReply = "Send me preferences (as a bitmap)";
+                    dataOutputStream.writeUTF(msgReply);
+
+                    //android sends a single byte (can extend if more modules added)
+                    
+                    //listens for preferences
+                    byte bitmap;
+                    bitmap = dataInputStream.readByte();
+
+                   
+                    //get number of 1's present in bitmap, this is number of modules requested
+                    byte bitmapCopy;
+                    int numberOfModulesRequested = 0;
+                    for (int i = 0; i < BITMAP_SIZE; i++) {
+                        bitmapCopy = bitmap;
+                        bitmapCopy >>>= i;
+                        if ((bitmapCopy & 1) == 1) {
+                            numberOfModulesRequested++;
+                        }
+                    }
+                    
+                    String[] modules = new String[numberOfModulesRequested - 1];
+                    
+                    //TODO translate 1's from bitmap into string array - mostly for debugging
+
+                    //get number of modules requested -this is the number of 1's present in the bitmap passed by android app 
+                    int moduleCounter = 0;
+                    for (int i = 0; i < BITMAP_SIZE; i++) {
+                        bitmapCopy = bitmap;
+                        bitmapCopy >>>= i;
+                        if ((bitmapCopy & 1) == 1) {
+                            switch (i) {
+                                case 0:
+                                    modules[moduleCounter] = "";
+                                    moduleCounter++;
+                                    break;
+                                case 1:
+                                    modules[moduleCounter] = "";
+                                    moduleCounter++;
+                                    break;
+                                case 2:
+                                    modules[moduleCounter] = "";
+                                    moduleCounter++;
+                                    break;
+                                case 3:
+                                    modules[moduleCounter] = "";
+                                    moduleCounter++;
+                                    break;
+                                case 4:
+                                    modules[moduleCounter] = "";
+                                    moduleCounter++;
+                                    break;
+                                case 5:
+                                    modules[moduleCounter] = "";
+                                    moduleCounter++;
+                                    break;
+                                case 6:
+                                    modules[moduleCounter] = "";
+                                    moduleCounter++;
+                                    break;
+                                case 7:
+                                    modules[moduleCounter] = "";
+                                    moduleCounter++;
+                                    break;
+                                default:
+                                    System.out.println("Error in determing requested modules.");
+                            }
+                        }
+                    }
+
+                    //use string array to start code requesting individual modules
+                    for (int i = 0; i < numberOfModulesRequested; i++) {
+                        switch (modules[i]) {
+                            case "Twitter":
+                                //TODO send request for Twitter info to android
+                                break;
+                            case "Weather":
+                                //TODO send request for weather to ---android or weather api
+                                break;
+                            case "Quote":
+                                //TODO send request for quote
+                                break;
+                        }
+                    }*/
+                    
+                    //---------------------------------------------------------------------------------
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                final String errMsg = e.toString();
+
+                System.out.println(errMsg);
+
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataInputStream != null) {
+                    try {
+                        dataInputStream.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataOutputStream != null) {
+                    try {
+                        dataOutputStream.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+    }
+
+    private static String getIpAddress() {
+        String ip = "";
+        try {
+            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = enumNetworkInterfaces
+                        .nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface
+                        .getInetAddresses();
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress.nextElement();
+
+                    if (inetAddress.isSiteLocalAddress()) {
+                        ip += "SiteLocalAddress: "
+                                + inetAddress.getHostAddress() + "\n";
+                    }
+
+                }
+
+            }
+
+        } catch (SocketException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            ip += "Something Wrong! " + e.toString() + "\n";
+        }
+
+        return ip;
     }
     
 }
--- Test_Connection/THISVERSIONWORKS/ServerRaspberry/raspberryPiServer/src/raspberrypiserver/RaspberryPiServer.java
+++ Test_Connection/THISVERSIONWORKS/ServerRaspberry/raspberryPiServer/src/raspberrypiserver/RaspberryPiServer.java
@@ -153,6 +153,7 @@ public class RaspberryPiServer {
                                 break;
                         }
                     }
+                    
                     //---------------------------------------------------------------------------------
                 }
             } catch (IOException e) {
