32550ebaaa39aec85f4a035a0daa3640cfe45d77
snowman1ng <tue99667@temple.edu>
2016-03-20 15:02:38 -0400

    Integrated bluetooth connectivity into magicmirror app.

--- magicMirror/app/src/main/java/fairestintheland/magicmirror/MainActivity.java
+++ magicMirror/app/src/main/java/fairestintheland/magicmirror/MainActivity.java
@@ -1,10 +1,20 @@
 package fairestintheland.magicmirror;
 
+import android.annotation.TargetApi;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -30,6 +40,8 @@ import org.json.JSONObject;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
@@ -37,6 +49,7 @@ import java.net.SocketAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     //UI objects
@@ -60,6 +73,29 @@ public class MainActivity extends AppCompatActivity {
     JSONArray parcel;
     Context context;
     ConnectivityManager cm;
+
+    BluetoothSocketConnection bluetoothSocketConnection;
+
+    //bluetooth vars
+    //int used to determine what activity was presented in the onActivityResult() method
+    private static final int REQUEST_ENABLE_BT = 100;
+    private String raspberryPiName;
+    //string defined on android and raspberry sides to establish connection
+    private final String UUIDSTRING = "a96d5795-f8c3-4b7a-9bad-1eefa9e11a94";
+    BluetoothManager bluetoothManager;
+    BluetoothAdapter bluetoothAdapter;
+    TextView bluetoothInfo;
+    ListView discoverableList;
+    boolean bluetoothAvailable;
+    IntentFilter filter;
+    BluetoothDevice device;
+    ArrayList<Parcelable> devices = new ArrayList<>(); //hostname and MAC address of every device that is discoverable
+    BluetoothSocket clientSocket = null;
+    UUID uuid;
+    InputStream clientSocketInputStream;
+    OutputStream clientSocketOutputStream;
+    EditText raspberryNameEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -127,7 +163,213 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /*//create BluetoothSocketConnection object to start services and manage connection
+        //do preliminary setup that must happen in a class with an activity
+        bluetoothSocketConnection = new BluetoothSocketConnection(context);
+        // Register the BroadcastReceiver
+        bluetoothSocketConnection.filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(bluetoothSocketConnection.getmReceiver(), bluetoothSocketConnection.filter);
+        // Don't forget to unregister during onDestroy
+        bluetoothSocketConnection.bluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);*/
+        //above was trying to put bluetooth code in a separate class
+
+        //discoverableList = (ListView) findViewById(R.id.discoverableList);
+        bluetoothInfo = (TextView) findViewById(R.id.bluetoothInfo);
+        raspberryNameEditText = (EditText) findViewById(R.id.raspberryName);
+
+        new BluetoothAsync().execute();
+
     }
+
+    public void startBluetoothActivity() {
+        //startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+    }
+
+    //----------BLUETOOTH SECTION-------------------------------------------------------------------------
+    /**
+     * Performed asynchronously to set up bluetooth services in the background. This overrides the
+     * doInBackground and onPostExecute methods. It checks to see if bluetooth is supported on the
+     * android device first. If so, it checks to see whether bluetooth is enabled. If bluetooth is
+     * not enabled, it prompts to=he user with an activity so they can turn on bluetooth if desired.
+     * It also provides output to Sys.out for the programmer through the process.
+     */
+    public class BluetoothAsync extends AsyncTask<Void, Void, Void> {
+        BluetoothAsync() {}
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            setupBluetooth();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            //textResponse.setText(response);
+            System.out.println("BluetoothAsync complete.");
+            super.onPostExecute(result);
+        }
+    }
+
+    /**
+     * Create a BroadcastReceiver for ACTION_FOUND, this is called every time a new device is found
+     * during device discovery. If a name matching the string defined by raspberryPiName, which the
+     * user can enter via am EditText object, tryToConnect() is called to create a BluetoothSocket
+     * and attempt to connect to a listening ServerBluetoothSocket. If the uesr does not enter the
+     * remote hostname correctly, tryToConnect() will not successfully connect.
+     */
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                devices.add(intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE));
+                // Add the name and address to an array adapter to show in a ListView
+                System.out.println("Found " + device.getName() + " at " + device.getAddress());
+                //discoverableList.add(device.getName() + "\n" + device.getAddress());
+                System.out.println(devices);
+
+                //try a connection based on device name
+                if (device.getName().equals(raspberryPiName)) {
+                    tryToConnect();
+                    return;
+                }
+            }
+        }
+    };
+
+    /**
+     * Checks to see if bluetooth is available and enabled on the phone, if it is not enabled,
+     * user will be presented with an activity to turn on bluetooth ,the response will be handled
+     * by onActivityResult
+     */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
+    public void setupBluetooth() {
+        // Register the BroadcastReceiver
+        filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+
+        bluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+        if (bluetoothAdapter == null) { //bluetooth not supported
+            bluetoothInfo.setText("Bluetooth unavailable on this device. Sorry.");
+        } else {
+            if (!bluetoothAdapter.isEnabled()) {
+                //presents activity to ask user to turn on bluetooth, response processed by onActivityResult()
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            } else {
+                bluetoothInfo.setText("Bluetooth available and on. Great.");
+            }
+
+            //UUID is how the android app finds the raspberry app, this is also defined on the raspberry side
+            uuid = UUID.fromString(UUIDSTRING);
+        }
+    }
+
+    /**
+     * Attempt a bluetooth connection to a device whose name is known from the device discovery process.
+     * The device hostname is input by the user and this method is only called if that user input String
+     * matches a device hostname found in the discovery process. This method establishes a BluetoothSocket
+     * using createRfcommSocketToServiceRecord() and tries to connect to a listening BluetoothServerSocket
+     * that uses the same UUID.
+     */
+    private void tryToConnect() {
+        BluetoothDevice btDevice = device;
+        System.out.println("Trying to connect to " + raspberryPiName);
+
+        btDevice = bluetoothAdapter.getRemoteDevice(btDevice.getAddress());
+
+        try {
+            clientSocket = btDevice.createRfcommSocketToServiceRecord(uuid);
+            clientSocket.connect();
+            System.out.println("Connected to raspberry pi.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        //write content to the socket if the socket was opened successfully
+        writeContentToSocket(clientSocket);
+    }
+
+    /**
+     *
+     * @param clientSocket an open BluetoothSocket object
+     */
+    public void writeContentToSocket(BluetoothSocket clientSocket) {
+        byte[] buffer = new byte[1024];  // buffer store for the stream
+        int bytes; // bytes to send to write()
+
+        try {
+            buffer = raspberryPiName.getBytes();
+            clientSocketInputStream = clientSocket.getInputStream();
+            clientSocketOutputStream = clientSocket.getOutputStream();
+            clientSocketOutputStream.write(buffer);
+            System.out.println("Wrote " + new String(buffer) + " to raspberry.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Responds to sync button press, starts discovery of remote bluetooth devices that are set to
+     * discoverable. If the default string was not changed, the user is prompted to input a valid
+     * hostname.
+     *
+     * @param view
+     */
+    public void startLooking(View view) {
+        raspberryPiName = raspberryNameEditText.getText().toString().trim();
+
+        //only start discovery if user has entered a remote hostname
+        if (!raspberryPiName.equals("Enter raspberry computer name here")) {
+            if (!raspberryPiName.equals("Please reenter raspberry pi bluetooth name")) {
+                if (!raspberryPiName.isEmpty()) {
+                    //if false, bluetooth off, otherwise start discovery, when results arrive the callback is BroadcastReceiver
+                    bluetoothAvailable = bluetoothAdapter.startDiscovery();
+                } else {
+                    raspberryNameEditText.setText("Please reenter raspberry pi bluetooth name");
+                }
+            } else {
+                raspberryNameEditText.setText("Please reenter raspberry pi bluetooth name");
+            }
+        } else {
+            raspberryNameEditText.setText("Please reenter raspberry pi bluetooth name");
+        }
+    }
+
+    /**
+     * Respond to a user's interacting with an activity presented to turn on bluetooth capabilities.
+     *
+     * @param requestCode Defined in the calling code, allows response to multiple events,
+     *                    REQUEST_ENABLE_BT represents a bluetooth activity request
+     * @param resultCode Results of user's interaction with the activity
+     * @param data
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //responds to the user's response to the bluetooth enable prompt if it was presented
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == RESULT_OK) {
+                bluetoothInfo.setText("You enabled bluetooth. Thanks.");
+            } else {
+                bluetoothInfo.setText("Bluetooth disabled.");
+            }
+        }
+    }
+
+    /**
+     * Overridden to unregister the BroadcastReceiver.
+     */
+    @Override
+    protected void onDestroy() {
+        //have to unregister BroadcastReceiver before the app closes
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+    //-----------------END BLUETOOTH SECTION-----------------------------------------------------------------
 
 
 
@@ -440,4 +682,36 @@ public class MainActivity extends AppCompatActivity {
             return v;
         }
     }
+
+    /**
+     * Overridden to unregister the BroadcastReceiver.
+     */
+    /*@Override
+    protected void onDestroy() {
+        unregisterReceiver(bluetoothSocketConnection.getmReceiver());
+        super.onDestroy();
+    }*/
+
+
+    /**
+     * Respond to a user's interacting with an activity presented to turn on bluetooth capabilities.
+     * This will alert the user as to whether they did or did not successfully enable bluetooth when
+     * called with requestCode == bluetoothSocketConnection.getREQUEST_ENABLE_BT()
+     *
+     * @param requestCode Defined in the calling code, allows response to multiple events,
+     *                    REQUEST_ENABLE_BT represents a bluetooth activity request
+     * @param resultCode Results of user's interaction with the activity
+     * @param data
+     */
+    /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //responds to the user's response to the bluetooth enable prompt if it was presented
+        if (requestCode == bluetoothSocketConnection.getREQUEST_ENABLE_BT()) {
+            if (resultCode == RESULT_OK) {
+                //TODO change ref - bluetoothInfo.setText("You enabled bluetooth. Thanks.");
+            } else {
+                //TODO cheange ref - bluetoothInfo.setText("Bluetooth disabled.");
+            }
+        }
+    }*/
 }
