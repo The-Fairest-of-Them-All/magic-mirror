d7d91af2088b3ed94379eba6de4154f6c6088a65
snowman1ng <tue99667@temple.edu>
2016-03-23 11:53:10 -0400

    Adds a test project to test invoking cmd with args from a java app. This simple version that performs ls -la works.

---
+++ InvokeCommandLine/src/invokecommandline/InvokeCommandLine.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package invokecommandline;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Owner
+ */
+public class InvokeCommandLine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            String[] commandAndArgs = {"ls", "-la"};
+            Process p = Runtime.getRuntime().exec(commandAndArgs);
+            p.waitFor();
+            
+            
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(InvokeCommandLine.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(InvokeCommandLine.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
