ff553f3f27b694cc98f4b1d24d643b26d7816c8c
snowman1ng <tue99667@temple.edu>
2016-04-05 19:15:33 -0400

    Changed jar dependency to a custom jar conaining the com.intel.bluetooth class files necessary for both linux and iwndows.

--- RpiUI/test/bluetooth/BluetoothListenerThreadTest.java
+++ RpiUI/test/bluetooth/BluetoothListenerThreadTest.java
@@ -52,6 +52,6 @@ public class BluetoothListenerThreadTest {
         BluetoothListenerThread instance = null;
         instance.run();
         // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        fail("The test case is a prototype.");
     }
 }
