bc07276c5ce2c8bf3808858c216bdbf365f21864
yogurtmaker <yaotian2011@hotmail.com>
2016-04-06 18:02:47 -0400

    junit test case for calendar event

--- magicMirror/app/src/androidTest/java/fairestintheland/magicmirror/MainActivityTest.java
+++ magicMirror/app/src/androidTest/java/fairestintheland/magicmirror/MainActivityTest.java
@@ -1,16 +1,41 @@
 package fairestintheland.magicmirror;
 
-import org.junit.Test;
+import android.content.Intent;
+import android.test.ActivityUnitTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.view.ContextThemeWrapper;
 
-/**
- * Created by Raymond on 4/6/2016.
- */
-public class MainActivityTest
-{
+import junit.framework.Assert;
 
-    @Test
-    public void testSetupBluetooth()
-    {
 
+public class MainActivityTest extends ActivityUnitTestCase<MainActivity> {
+    MainActivity mainActivity;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
     }
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        ContextThemeWrapper context = new ContextThemeWrapper(getInstrumentation().getTargetContext(), R.style.AppTheme);
+        setActivityContext(context);
+        startActivity(new Intent(getInstrumentation().getTargetContext(), MainActivity.class), null, null);
+        mainActivity = (MainActivity)getActivity();
+    }
+
+    @SmallTest
+    public void testGetCalendar() throws Exception
+    { CalendarEvent calendar = new CalendarEvent();
+        String str= calendar.readCalendarEvent(mainActivity).toString();
+        Assert.assertEquals("[Event:event1, Description:]",str);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+
+
+
 }
